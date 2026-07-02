b18406eb7e2842d1edf32b15e20802fd001aa8fd
Umer Iqbal <official.umer.iqbal@gmail.com>
2026-07-02 17:25:22 +0500

    Make section files and setup custom color in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
-      <h1 className="font-bold text-2xl">Single Page Application</h1>
+      <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-size-[16px_16px]"></div>
+      <Navbar />
+      <Hero />
+      <Contact />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <>
+            <div>Hero Section</div>
+            <h1 className="font-bold text-2xl text-custom-blue">Single Page Application</h1>
+        </>
+    )
+}
+
+export default Hero
